b5db6e90e974412e0480a466b4d8476f2653ad31
noam.arie <noamhazilon12@gmail.com>
2026-01-16 14:04:41 +0200

    Add currency formula

---
+++ currency.rb
@@ -0,0 +1,17 @@
+class Currency < Formula
+  desc "Currency conversion CLI tool"
+  homepage "https://github.com/NoamArie12/Currency-CLI"
+  url "https://github.com/NoamArie12/Currency-CLI/releases/download/v1.0.0/cur"
+  sha256 "b369ab35e9c9dfe24322d04cb2ccd23724946d115acd11ef586ec38d2f6ca9c6" # <--- Put the hash from the command above here
+  version "1.0.0"
+
+  def install
+    # This takes the file 'cur' and installs it as 'currency'
+    bin.install "cur" => "currency"
+  end
+
+  test do
+    # Simple check to see if it runs
+    system "#{bin}/currency", "--help"
+  end
+end
